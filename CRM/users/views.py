3fee3960aa3eed792ba5699a6ef1ce132357c2cf
DawidGaw <dawidgaworski@onet.pl>
2026-03-21 10:00:03 +0100

    login template added and user application created

---
+++ CRM/users/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def login_view(request):
+    return render(request, 'users/login.html')
+
+
+def index_view(request):
+    return render(request, 'users/index.html')
+
+
