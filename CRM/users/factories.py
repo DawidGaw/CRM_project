6a0e5047a89d6a25ed21d6f578a9d29daac28252
DawidGaw <dawidgaworski@onet.pl>
2026-08-10 18:50:23 +0200

    fix: add username to UserFactory

--- CRM/users/factories.py
+++ CRM/users/factories.py
@@ -8,6 +8,7 @@ class UserFactory(DjangoModelFactory):
         model = User
         django_get_or_create = ("username",)
 
+    username = factory.Sequence(lambda n: f"user{n}")
     email = factory.Sequence(lambda n: f"user{n}@example.com")
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
